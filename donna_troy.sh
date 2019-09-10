2499c6fd846240811d7de2c5adc6aeec02fa5845
Willian B. Yamashita <willianby@gmail.com>
2019-09-10 23:58:30 +0000

    commig script cowsay

---
+++ donna_troy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+figlet COWSAY
+for COWSAY in $(ls /usr/share/cowsay/cows/)
+do
+	cowsay -f $COWSAY LINUX;
+	sleep 1;
+	clear;
+done
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
